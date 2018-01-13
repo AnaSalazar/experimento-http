02dece3a669a1e0710da774ba3c51c83f3e897dd
AnaSalazar <anastephany.salazarguzman@gmail.com>
2018-01-12 19:15:16 -0600

    Actualiza pasos para autenticación

--- js/app.js
+++ js/app.js
@@ -8,19 +8,32 @@ var config = {
   };
 firebase.initializeApp(config);
 
-/* AUTENTICACIÓN
-0 Para poder usar la autenticacion de firebase es necesario que enlazemos al hatml los otros cdn que necesitamos, mismo que vienen en la pagina
+/*
+En la lectura anterior ya tenemos nuestro archivo incializado con Firebase,
+ahora vamos a subir nuestro proyecto a github y procedemos a trabajar con la
+autenticación a través de Google.
+
+AUTENTICACIÓN
+
+0 Para poder usar la autenticacion de firebase es necesario que enlazemos al html
+los otros cdn que necesitamos, mismos que vienen en la página https://firebase.google.com/docs/web/setup?authuser=0
 
 1 Crear botón para disparar el evento
 
-2 Me dirijo a mi proyecto, click en autenticacion, click en SIGN-IN METHOD y habilitamos google.
+2 Me dirijo a mi proyecto, click en autenticación, click en SIGN-IN METHOD y habilitamos google.
 
 3 Go to docs, Comienza a usar web, autenticacion, acceso con google, seguir instrucciones
 
 4 En nuestro app.js vamos a crear una instancia del objeto del proveedor de Google:
 var provider = new firebase.auth.GoogleAuthProvider();
-y nos saltamos hasta el inciso 5 porque los anteriores puntos de la documentacion son opcionales, ahi vemos que ya nos da unas lineas que debemos pegar en nuestro código
+y nos saltamos hasta el inciso 5 porque los anteriores puntos de la documentacion
+son opcionales, ahi vemos que ya nos da unas lineas que debemos pegar en nuestro código
 */
+
+// 5 Asi que nos vamos a nuestro archivo app.js y vamos a empezar por traernos el botón para asignarle un evento
+$('#buttonGoogle').click(function () {
+  authGoogle();
+});
 
 // 6 creamos nuestra función y ahí crearemos la instancia
 function authGoogle(e) {
@@ -29,7 +42,9 @@ function authGoogle(e) {
   authentication(provider);
 }
 
-// 7 Seguido de esto ya podemos pegar las lineas que firebase nos da, pero para no dejarlas "sueltas" en nuestro código las guaradamos dentro de una función que se llamara desde la anterior
+// 7 Seguido de esto ya podemos pegar las lineas que firebase nos da, pero para
+// no dejarlas "sueltas" en nuestro código las guardamos dentro de una función
+// que se llamara desde la anterior
 function authentication(provider) {
   firebase.auth().signInWithPopup(provider)
     .then(function(result) {
@@ -49,13 +64,13 @@ function authentication(provider) {
       console.log(credential);
     });
 }
-// 5 Asi que nos vamos a nuestro archivo app.js y vamos a empezar por traernos el botón para asignarle un evento
-$('#buttonGoogle').click(function () {
-  authGoogle();
-});
 
+// 8 Si nosotros intentamos correr esto nos va a arrojar un error, así que lo
+// siguiente que haremos es crear una rama gh-pages y correrlo ahí. Ahora nos muestra
+// un error diferente realizamos los pasos que nos indica y Listo, ya funciona.
 
 // BASE DE DATOS
+
 // 1 Nos traemos el elemento sobre el que queremos probar nuestra base de datos
 var $title = $('#title');
 
@@ -74,3 +89,6 @@ database.on('value', function(snapshot) {
 
 // 6 Volvemos a la base de datos y agregamos una identificador en Name
 // le ponemos text y en Value escribimos lo que queremos que se guarde
+
+
+// 7 ya podemos ver en nuestro navegador que funciona a la perfección
